8615d42574dd2cd38f65494c799081a24ebe5cf4
Mauro Talevi <mauro.talevi@aquilonia.org>
2012-07-09 20:04:14 +0200

    Assume mvn is in path.

--- jbehave-eclipse/upload-repository.sh
+++ jbehave-eclipse/upload-repository.sh
@@ -23,7 +23,7 @@ if [ "$PATH" != "" ] ; then
 fi
 VERSIONED_REFERENCE="$REFERENCE/$VERSION"
 
-$MVN_HOME/bin/mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:get -Dartifact=$ARTIFACT_FULL -Dtransitive=false -Ddest=target/$ZIPPED_ARTIFACT
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:get -Dartifact=$ARTIFACT_FULL -Dtransitive=false -Ddest=target/$ZIPPED_ARTIFACT
 
 /usr/bin/scp target/$ZIPPED_ARTIFACT jbehave.org:uploads/
 /usr/bin/ssh jbehave.org "rm -rf $VERSIONED_ARTIFACT; unzip -q -d $VERSIONED_ARTIFACT uploads/$ZIPPED_ARTIFACT; rm -r $VERSIONED_REFERENCE; mv $VERSIONED_ARTIFACT $VERSIONED_REFERENCE; cd $REFERENCE; rm $QUALIFIER; ln -s $VERSION $QUALIFIER"
